ab700407b10653fd55ec4e330f8894ccb0c66dce
EC2 Default User <ec2-user@ip-172-31-75-159.ec2.internal>
2021-03-15 16:01:03 +0000

    fix:new api

--- rss/client.py
+++ rss/client.py
@@ -8,48 +8,32 @@ import tqdm
 import zarr
 
 
-def load_inline(seismic, scalers, bounds, line_number, mask_val=np.nan):
+def load_line(seismic, scalers, bounds, line_number, mask_val=np.nan, sort_order='inline'):
+    sort_order = sort_order.lower()
+    if(sort_order not in ('inline', 'crossline')):
+        raise RuntimeError(
+            f'{sort_order} not supported, sort order should be on of inline or crossline.')
+    
     min_inline, min_crossline, max_inline, max_crossline = bounds
+
+    if(sort_order == 'inline'):
+        min_line = min_inline        
+        max_line = max_inline
+    else:
+        min_line = min_crossline        
+        max_line = max_crossline
     
-    if(line_number < min_inline or line_number > max_inline):
-        min_inline = min_inline        
-        max_inline = max_inline
-        raise RuntimeError(f'{line_number} out of bounds [{min_inline}, {max_inline}].')
+    if(line_number < min_line or line_number > max_line):
+        raise RuntimeError(f'{line_number} out of bounds [{min_line}, {max_line}].')
     
-    traces = seismic[:,:,line_number - min_inline]
+    traces = seismic[:,:,line_number - min_line]
     mask = traces < 1
-    min_val, max_val = scalers[line_number - min_inline,:]
+    min_val, max_val = scalers[line_number - min_line,:]
     traces -= 1
     traces = traces.astype(np.float32) * max_val/(65535 - 1)
     traces += min_val
     traces[mask] = mask_val
     return traces, mask
-
-
-def load_crossline(seismic, scalers, bounds, line_number, mask_val=np.nan):
-    min_inline, min_crossline, max_inline, max_crossline = bounds
-
-    if(line_number < min_crossline or line_number > max_crossline):
-        min_crossline = min_crossline      
-        max_crossline = max_crossline
-        raise RuntimeError(f'{line_number} out of bounds [{min_crossline}, {max_crossline}].')
-    
-    def rescale(trace, min_val, max_val):
-        mask = trace < 1
-        trace = np.copy(trace)
-        trace -= 1
-        trace = trace.astype(np.float32) * max_val/(65535 - 1)
-        trace += min_val
-        return trace, mask
-
-    x = seismic[:,line_number-min_crossline,:]
-    y = list(chain(*[rescale(x[:,i], *scalers[i,:]) for i in 
-                         range(0, max_inline - min_inline+1)]))
-    crossline = np.vstack(y[::2])
-    mask = np.vstack(y[1::2])
-    crossline[mask] = mask_val
-    
-    return crossline, mask
 
 class ZArrFromFile():
     def __init__(self, filename):
@@ -60,11 +44,8 @@ class ZArrFromFile():
         self.scalers = root["scalers"]
         self.bounds = root["bounds"]
             
-    def inline(self, line_number):
-        return load_inline(self.seismic, self.scalers, self.bounds, line_number)
-
-    def crossline(self, line_number):
-        return load_crossline(self.seismic, self.scalers, self.bounds, line_number)
+    def line(self, line_number, sort_order='inline'):
+        raise NotImplementedError()
 
 class ZArrFromS3():
     def __init__(self, filename, client_kwargs):
@@ -75,15 +56,15 @@ class ZArrFromS3():
 
         self.root = root
         self.bounds = self.root["bounds"]
-           
+
     def line(self, line_number, sort_order='inline'):
         sort_order = sort_order.lower()
         if(sort_order not in ('inline', 'crossline')):
-        raise RuntimeError(
-            f'{sort_order} not supported, sort order should be on of inline or crossline.')        
+            raise RuntimeError(
+                f'{sort_order} not supported, sort order should be on of inline or crossline.')
 
         root = self.root[sort_order]
         seismic = root["seismic"]
         scalers = root["scalers"]
 
-        return load_inline(seismic, scalers, self.bounds, line_number)
+        return load_line(seismic, scalers, self.bounds, line_number, sort_order=sort_order)
